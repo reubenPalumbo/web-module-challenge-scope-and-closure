46e5bbbe8f391a28c517eebfdca60e585f2d1ea9
Reuben Palumbo <reubensandwichthe1st@gmail.com>
2020-08-17 11:38:00 -0800

    Checkpoint 1 - Finished

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ The module challenge is the afternoon project or assignment that students work t
 ## Objectives
 
 - Explain function scope
-- Describe what closure is, how closure is created in a program and why it is important to understand closures in JavaScript  
+- Describe what closure is, how closure is created in a program and why it is important to understand closures in JavaScript
 
 ## Introduction
 
@@ -19,7 +19,7 @@ This challenge focuses on both scope and closures.
 
 In this challenge you will be working to build a `scoreboard` (in the console) that takes randomly generated data and keeps track of a game's progress. If you're not familiar with the rules of baseball what you need to know is this: there are 9 innings and teams take turns "at-bat." Teams can only score while they are at bat. A team stops being at bat once they have gotten 3 `outs` by either striking out or through game play. You can read more about baseball rules [here](https://www.rulesofsport.com/sports/baseball.html).
 
-A scoreboard in a major league stadium looks something like this. In fact, the scoreboard at Fenway Park in Boston is actually quite famous. 
+A scoreboard in a major league stadium looks something like this. In fact, the scoreboard at Fenway Park in Boston is actually quite famous.
 
 ![Fenway Scoreboard](https://storage.googleapis.com/afs-prod/media/media:e959506330fd4e5890023c93cfbaac55/800.jpeg)
 
@@ -44,29 +44,33 @@ Find the file `index.js` and complete the tasks.
 Edit the `ReadMe` file with your answers.
 
 1. In your own words, define closure (1-2 sentences).
+   The ability for children of functions to get varibles outside of thier own block.
+
 2. Study the following code, then answer the questions below.
 
 ```js
-function personalDice(name){
-  return function(){
-      // generate random number between 1 and 6
+function personalDice(name) {
+  return function () {
+    // generate random number between 1 and 6
     const newRoll = Math.floor(Math.random() * 6);
-    console.log(`${name} rolled a ${newRoll}`)
-  }
+    console.log(`${name} rolled a ${newRoll}`);
+  };
 }
 
 const dansRoll = personalDice("Dan");
 
 const zoesRoll = personalDice("Zoe");
 
-
 dansRoll();
 dansRoll();
 ```
 
 a. Where is closure used in this code? How can you tell?
+"Name" is the closure, the "name" is only set in the parameter of the function personalDice(), the child function is able to grab that paramter and use it.
 b. Compare and contrast calling `dansRoll` the first and second time. What is always the same? What could change?
-c. What is the lexical scope of `newRoll`? 
+The name is always the same, and the changing var is the newRoll.
+c. What is the lexical scope of `newRoll`?
+Anything outside of the anonymous function.
 
 ### Task 2c - Exit Ticket
 
@@ -83,11 +87,11 @@ See if you can complete one or more of the following challenges:
 1. Predict the output of the code below and explain why this is the output using what you learned today. When you're ready for answers, view an explanation [here](https://www.coderbyte.com/algorithm/3-common-javascript-closure-questions
 
 ```js
-(function(){
-  var a = b = 3;
+(function () {
+  var a = (b = 3);
 })();
-console.log("a defined? " + (typeof a !== 'undefined'));
-console.log("b defined? " + (typeof b !== 'undefined'));
+console.log("a defined? " + (typeof a !== "undefined"));
+console.log("b defined? " + (typeof b !== "undefined"));
 ```
 
 2. Write a function that would allow you to do this using a closure. (This is another interview question we've seen before - when you're ready for answers, view an explanation [here](https://www.coderbyte.com/algorithm/3-common-javascript-closure-questions)).
@@ -110,6 +114,6 @@ addSix(21); // returns 27
 
 Follow these steps for completing your project.
 
-- [ ] Submit a pull request to merge <firstName-lastName> Branch into master (student's  Repo). **Please don't merge your own pull request**
+- [ ] Submit a pull request to merge <firstName-lastName> Branch into master (student's Repo). **Please don't merge your own pull request**
 - [ ] Add your Team Lead as a reviewer on the pull request
 - [ ] Your Team Lead will count the project as complete by merging the branch back into master
--- index.js
+++ index.js
@@ -2,44 +2,46 @@
 
 /**
  * ### Challenge `processFirstItem`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processFirstItem`.
  * It takes two arguments:
  * @param stringList an array of strings.
  * @param callback function that takes a string as its argument.
  * @returns the result of invoking `callback` with the FIRST element in `stringList`.
- * 
+ *
  * Example of usage of this higher-order function:
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
-*/
+ */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
 /* Task 1: `counterMaker`
  * Study the code for counter1 and counter2. Answer the questions below.
- * 
- * 1. What is the difference between counter1 and counter2?
- * 
- * 2. Which of the two uses a closure? How can you tell?
- * 
- * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
-*/
+ * 1. What is the difference between counter1 and counter2?
+ *  counter1 has all the vars stored in the function, keep the data contained and only accessable through the function. counter2 has a global var and is changing count globally.
+ *
+ * 2. Which of the two uses a closure? How can you tell?
+ *  counter1 uses closure, it has a nested function that is calling the count from the parent function.
+ * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better?
+ *  counter1 is good for when you have multiple counters, say one for each in a set of 4 teams.
+ *  counter2 would be used for if the count need to be kept know to many other objects/functions. Could be a score of a game overall, something that can be changed by many different functions,
+ *
+ */
 
 // counter1 code
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
-  }
+    return count++;
+  };
 }
 
 const counter1 = counterMaker();
@@ -51,20 +53,26 @@ function counter2() {
   return count++;
 }
 
-
 /* Task 2: inning() 
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning() {
+  return function () {
+    const score = Math.floor(Math.random() * 3);
+    return score;
+  };
 }
+
+const team1 = inning();
+
+// console.log(team1());
 
 /* Task 3: finalScore()
 
-Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
+Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings 
+
+and returns the final score of the game in the form of an object.
 
 For example, 
 
@@ -73,14 +81,20 @@ finalScore(inning, 9) might return:
   "Home": 11,
   "Away": 5,
 }
+*/
 
-*/ 
-
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
-
+function finalScore(cb, innNum) {
+  temp = cb();
+  let score1 = 0;
+  let score2 = 0;
+  for (let i = 0; i < innNum; i++) {
+    score1 += temp();
+    score2 += temp();
+  }
+  return { Home: score1, Away: score2 };
 }
+
+// console.log(finalScore(inning, 9));
 
 /* Task 4: 
 
@@ -90,7 +104,7 @@ Create a function called `scoreboard` that accepts the following parameters:
 (2) Callback function `inning`
 (3) A number of innings
 
-and returns the score at each pont in the game, like so:
+and returns the score at each point in the game, like so:
 1st inning: awayTeam - homeTeam
 2nd inning: awayTeam - homeTeam
 3rd inning: awayTeam - homeTeam
@@ -102,9 +116,18 @@ and returns the score at each pont in the game, like so:
 9th inning: awayTeam - homeTeam
 Final Score: awayTeam - homeTeam */
 
+function scoreboard(cb, innNum) {
+  temp = cb();
+  let scoreAway = 0;
+  let scoreHome = 0;
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+  for (let i = 0; i < innNum; i++) {
+    scoreAway += temp();
+    scoreHome += temp();
+    console.log(
+      `Inning ${i + 1}: Away Team: ${scoreAway} - Home Team: ${scoreHome}`
+    );
+  }
 }
 
-
+scoreboard(inning, 9);
